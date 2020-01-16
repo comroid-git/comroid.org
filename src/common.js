af8835d36c3e17f7a3fa6ba9cd581902d57d6f37
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-01-16 18:33:40 +0100

    thx to shyCyanon#1707

--- src/common.js
+++ src/common.js
@@ -1,4 +1,9 @@
 function load() {
-    if (Math.random() < 0.05)
+    let random = Math.random();
+    if (random < 0.05)
         document.getElementById('error').textContent = '///Under Construxion///';
+    else if (random < 0.001) {
+        document.getElementById('error').textContent = 'Under your Bed';
+        document.getElementById('info').textContent = 'I am under your Bed';
+    }
 }
