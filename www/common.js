4c742d60fa33b91f194b4be333e9fa09ce3a6691
Daniel Padberg <dpadberg@uni-bremen.de>
2020-05-27 18:28:14 +0200

    Blueprint of a new design / extension of the construction page; optimization for mobile devices

--- www/common.js
+++
@@ -1,9 +0,0 @@
-function load() {
-    let random = Math.random();
-    if (random < 0.05)
-        document.getElementById('error').textContent = '///Under Construxion///';
-    else if (random < 0.001) {
-        document.getElementById('error').textContent = 'Under your Bed';
-        document.getElementById('info').textContent = 'I am under your Bed';
-    }
-}
