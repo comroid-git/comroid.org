96b519c1141e1542c06aeff8a4d6f25bd8cea29f
padbeda <danielpdbg@gmail.com>
2020-06-01 13:13:38 +0200

    Basic JSON approach
    A basic approach for the API interaction.
    - JSON data is fetched from an URL, currently a test file on a test server
    - Flex container is filled with boxes depending on the delivered JSON data

--- status/src/web/common.js
+++ status/src/web/common.js
@@ -1,13 +1,47 @@
 /**
- * Inits the grid of the status page, should contain all calls of addBox
+ * Inits the grid of the status page, performs the JSON fetch.
  */
 function initGrid() {
-	var display_name = "Cobalton";
-	var status = "offline";
-	addBox("Status Server", status);
-	for (i = 0; i < 9; i++) {
-		addBox(display_name, status);
-	}
+
+    fetch('http://my-json-server.typicode.com/padbeda/testing/db')
+        .then(response => response.json())
+        .then(data => createBoxes(data));
+
+}
+
+/**
+ * Creates all boxes for the status page grid.
+ * 
+ * @param {Object} data The JSON data delivered by the Fetch API request
+ */
+function createBoxes(data) {
+    for (var i = 0; i < data.results.length; i++) {
+        var obj = data.results[i];
+        addBox(obj.display_name, determineStatus(obj.status));
+    }
+}
+
+/**
+ * Translates the Status Number to a String.
+ * A string is required to set the correct CSS classes and texts inside a service box.
+ * 
+ * @param {Number} number The status number delivered by the Fetch API request
+ */
+function determineStatus(number) {
+    switch (number) {
+        case 1:
+            return "offline";
+            break;
+        case 2:
+            return "maintenance";
+            break;
+        case 3:
+            return "busy";
+            break;
+        case 4:
+            return "online";
+            break;
+    }
 }
 
 /**
@@ -17,25 +51,25 @@ function initGrid() {
  * @param {string} status The current status of the service
  */
 function addBox(display_name, status) {
-	// Create a new box in the status grid
-	var newDiv = document.createElement("div");
-	if (display_name == "Status Server") {
-		newDiv.id = "head_wrapper";
-	} else {
-		newDiv.className = "cell";
-	}
-	
-	// Add text and status div depending on previous JSON parsing
-	var statusDiv = document.createElement("div");
-	var statusText = document.createElement("p");	
-	statusText.appendChild(document.createTextNode(display_name + ":"));
-	statusDiv.className = status;
-	statusDiv.innerHTML = status.toUpperCase();
-	
-	// Append text and status div to the box
-	newDiv.appendChild(statusText);
-	newDiv.appendChild(statusDiv);
-	
-	// Add the box to the flex container
-	document.getElementById('flex_container').appendChild(newDiv);
+    // Create a new box in the status grid
+    var newDiv = document.createElement("div");
+    if (display_name == "Status Server") {
+        newDiv.id = "head_wrapper";
+    } else {
+        newDiv.className = "cell";
+    }
+
+    // Add text and status div depending on previous JSON parsing
+    var statusDiv = document.createElement("div");
+    var statusText = document.createElement("p");
+    statusText.appendChild(document.createTextNode(display_name + ":"));
+    statusDiv.className = status;
+    statusDiv.innerHTML = status.toUpperCase();
+
+    // Append text and status div to the box
+    newDiv.appendChild(statusText);
+    newDiv.appendChild(statusDiv);
+
+    // Add the box to the flex container
+    document.getElementById('flex_container').appendChild(newDiv);
 }
