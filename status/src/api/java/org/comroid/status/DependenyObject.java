0175170f8b278ded1637944767814c1608f6cb8b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-06-03 14:17:29 +0200

    added ListService request + response

--- status/src/api/java/org/comroid/status/DependenyObject.java
+++ status/src/api/java/org/comroid/status/DependenyObject.java
@@ -1,5 +1,14 @@
 package org.comroid.status;
 
+import org.comroid.status.entity.Service;
+import org.comroid.uniform.SerializationAdapter;
+
 public interface DependenyObject {
+    SerializationAdapter<?, ?, ?> SERIALIZATION_ADAPTER = null;
+
     String URL_BASE = "https://api.status.comroid.org/";
+
+    static Service resolveService(DependenyObject dependenyObject, String name) {
+        return null; //todo
+    }
 }
