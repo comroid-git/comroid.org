49ffddb5d0043be614cf766448bbb8ad8a639e21
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-06-03 15:01:39 +0200

    Added adapters subclass

--- status/src/api/java/org/comroid/status/DependenyObject.java
+++ status/src/api/java/org/comroid/status/DependenyObject.java
@@ -6,14 +6,16 @@ import org.comroid.uniform.SerializationAdapter;
 import org.jetbrains.annotations.NotNull;
 
 public interface DependenyObject {
-    @SuppressWarnings("ConstantConditions") // must be defined by the user
-    @NotNull SerializationAdapter<?, ?, ?> SERIALIZATION_ADAPTER = null;
-    @SuppressWarnings("ConstantConditions") // must be defined by the user
-    @NotNull HttpAdapter HTTP_ADAPTER = null;
-
     String URL_BASE = "https://api.status.comroid.org/";
 
     static Service resolveService(DependenyObject dependenyObject, String name) {
         return null; //todo
+    }
+
+    final class Adapters {
+        @SuppressWarnings("ConstantConditions") // must be defined by the user
+        public static @NotNull SerializationAdapter<?, ?, ?> SERIALIZATION_ADAPTER = null;
+        @SuppressWarnings("ConstantConditions") // must be defined by the user
+        public static @NotNull HttpAdapter HTTP_ADAPTER = null;
     }
 }
