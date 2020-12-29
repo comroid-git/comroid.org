a50547fb15bcd7b0190cf9e6c60eb2695d7a3586
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-12-29 18:08:41 +0100

    added logging

--- status/src/server/java/org/comroid/status/server/DiscordBot.java
+++ status/src/server/java/org/comroid/status/server/DiscordBot.java
@@ -1,13 +1,16 @@
 package org.comroid.status.server;
 
+import com.google.common.flogger.FluentLogger;
 import org.comroid.crystalshard.AbstractDiscordBot;
 import org.comroid.crystalshard.DiscordAPI;
 import org.comroid.mutatio.ref.Reference;
 import org.comroid.status.DependenyObject;
 
 import java.io.IOException;
+import java.util.logging.Level;
 
 public final class DiscordBot extends AbstractDiscordBot {
+    public static final FluentLogger LOGGER = FluentLogger.forEnclosingClass();
     public static final DiscordAPI DISCORD_API;
     public static final Reference<String> token = Reference.create();
     public static final Reference<DiscordBot> instance = Reference.create();
@@ -16,16 +19,22 @@ public final class DiscordBot extends AbstractDiscordBot {
         DiscordAPI.SERIALIZATION = DependenyObject.Adapters.SERIALIZATION_ADAPTER;
         DISCORD_API = new DiscordAPI(DependenyObject.Adapters.HTTP_ADAPTER);
         token.onChange(newToken -> {
+            LOGGER.atInfo().log("New token found! Restarting Discord Bot...");
+
             try {
                 if (instance.isNonNull()) {
                     instance.assertion().close();
                     instance.unset();
                 }
             } catch (IOException e) {
+                LOGGER.at(Level.SEVERE)
+                        .withCause(e)
+                        .log("An error occurred closing the old Discord Bot");
                 throw new RuntimeException("Error occurred restarting Discord Bot", e);
             } finally {
                 DiscordBot bot = token.into(DiscordBot::new);
                 instance.set(bot);
+                LOGGER.atInfo().log("New Bot started");
             }
         });
     }
