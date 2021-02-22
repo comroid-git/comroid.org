5b7e9ef0a608f9e619d8bb13e85a902ccc19d9eb
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-02-22 20:41:29 +0100

    Create StatusConnection.cs

---
+++ status/csharp/status-api/StatusConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace org_comroid_status_api
+{
+    public sealed class StatusConnection
+    {
+        public static readonly string BaseUrl = "https://api.comroid.org/";
+        private readonly string _serviceName;
+        private readonly string _token;
+        private readonly RestClient _rest;
+
+        public Service OwnService { get; private set; }
+
+        public StatusConnection(string serviceName, string token = null)
+        {
+            _serviceName = serviceName;
+            _token = token;
+            _rest = new RestClient(BaseUrl);
+
+            Task<Service> req = RequestServiceByName(serviceName);
+            req.GetAwaiter().OnCompleted(() => OwnService = req.Result);
+        }
+
+        public async Task<Service> RequestServiceByName(string serviceName)
+        {
+            RestRequest req = new RestRequest($"service/{serviceName}", Method.GET, DataFormat.Json);
+
+            return _rest.Execute<Service>(req).Data;
+        }
+    }
+}
