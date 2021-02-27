d27580e00e4fd4aaa0069fb3ec4b049ffa03c40e
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-02-27 03:08:20 +0100

    update manifests

--- status/csharp/status-app/Properties/AssemblyInfo.cs
+++ status/csharp/status-app/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("status-app")]
-[assembly: AssemblyDescription("Status Display app for comroid Services")]
+[assembly: AssemblyTitle("comroid Status App")]
+[assembly: AssemblyDescription("Status Display App for comroid Services")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("comroid")]
 [assembly: AssemblyProduct("status-app")]
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden, 
 // indem Sie "*" wie unten gezeigt eingeben:
 // [Assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.1.1")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
