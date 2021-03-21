04a6c1e280d856b166a42e5ba57e79260b4c7d4a
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-03-21 18:31:15 +0100

    hmmmm

--- auth/src/main/java/org/comroid/auth/server/AuthServer.java
+++ auth/src/main/java/org/comroid/auth/server/AuthServer.java
@@ -58,7 +58,7 @@ public final class AuthServer implements ContextualProvider.Underlying {
             this.context = MASTER_CONTEXT.plus("NetBoxServer", executor);
 
             logger.debug("Starting Rest server");
-            this.rest = new RestServer(MASTER_CONTEXT, this.executor, URL_BASE, InetAddress.getLoopbackAddress(), PORT, Endpoint.values());
+            this.rest = new RestServer(MASTER_CONTEXT, this.executor, URL_BASE, OS.current == OS.WINDOWS ? InetAddress.getLoopbackAddress() : InetAddress.getLocalHost(), PORT, Endpoint.values());
         } catch (UnknownHostException e) {
             throw new AssertionError(e);
         } catch (IOException e) {
