96f0f4d3b5bbc1973a879c2cba217d5503e498c3
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-03-21 23:43:43 +0100

    add redirect to login page

--- auth/src/main/java/org/comroid/auth/server/Endpoint.java
+++ auth/src/main/java/org/comroid/auth/server/Endpoint.java
@@ -109,7 +109,8 @@ public enum Endpoint implements ServerEndpoint.This {
             AuthServer.instance.getUserManager().closeSession(session);
             REST.Header.List response = new REST.Header.List();
             response.add("Set-Cookie", String.format("%s=null", UserSession.COOKIE_PREFIX));
-            return new REST.Response(OK, response);
+            response.add(CommonHeaderNames.REDIRECT_TARGET, "login");
+            return new REST.Response(MOVED_PERMANENTLY, response);
         }
     },
     SESSION_DATA("session_data.js") {
