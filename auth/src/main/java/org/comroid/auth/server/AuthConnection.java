a75944a78b6b23c5948b1f63f2add3b38c195f7b
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-14 23:59:43 +0200

    use context

--- auth/src/main/java/org/comroid/auth/server/AuthConnection.java
+++ auth/src/main/java/org/comroid/auth/server/AuthConnection.java
@@ -1,5 +1,6 @@
 package org.comroid.auth.server;
 
+import org.comroid.api.ContextualProvider;
 import org.comroid.auth.user.UserSession;
 import org.comroid.mutatio.model.RefContainer;
 import org.comroid.restless.REST;
@@ -10,7 +11,6 @@ import org.comroid.webkit.socket.WebkitConnection;
 import org.java_websocket.WebSocket;
 
 import java.util.Optional;
-import java.util.concurrent.Executor;
 
 public final class AuthConnection extends WebkitConnection {
     private final UserSession session;
@@ -23,8 +23,8 @@ public final class AuthConnection extends WebkitConnection {
         return Optional.ofNullable(session);
     }
 
-    public AuthConnection(WebSocket socketBase, REST.Header.List headers, Executor executor) {
-        super(socketBase, headers, executor);
+    public AuthConnection(WebSocket socketBase, REST.Header.List headers, ContextualProvider context) {
+        super(socketBase, headers, context);
         UserSession session = null;
         try {
             session = UserSession.findSession(headers);
