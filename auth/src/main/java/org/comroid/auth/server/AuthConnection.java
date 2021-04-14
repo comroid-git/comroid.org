8f6cbe35334f290b51b929569660f11cf4bb0c44
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-14 15:57:08 +0200

    initialize user session

--- auth/src/main/java/org/comroid/auth/server/AuthConnection.java
+++ auth/src/main/java/org/comroid/auth/server/AuthConnection.java
@@ -1,13 +1,30 @@
 package org.comroid.auth.server;
 
+import org.comroid.auth.user.UserSession;
 import org.comroid.restless.REST;
+import org.comroid.restless.server.RestEndpointException;
 import org.comroid.webkit.socket.WebkitConnection;
 import org.java_websocket.WebSocket;
 
+import java.util.Optional;
 import java.util.concurrent.Executor;
 
 public final class AuthConnection extends WebkitConnection {
+    private final UserSession session;
+
+    public Optional<UserSession> getSession() {
+        return Optional.ofNullable(session);
+    }
+
     public AuthConnection(WebSocket socketBase, REST.Header.List headers, Executor executor) {
         super(socketBase, headers, executor);
+        UserSession session = null;
+        try {
+            session = UserSession.findSession(headers.toJavaHeaders());
+        } catch (RestEndpointException unauthorized) {
+            session = null;
+        } finally {
+            this.session = session;
+        }
     }
 }
