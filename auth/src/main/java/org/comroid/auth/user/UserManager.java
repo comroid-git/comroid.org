1146ce78d2ee1d0ec6fbbffe24ad8a4be5a5269a
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-22 20:09:07 +0200

    use data dir

--- auth/src/main/java/org/comroid/auth/user/UserManager.java
+++ auth/src/main/java/org/comroid/auth/user/UserManager.java
@@ -14,8 +14,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 public final class UserManager implements ContextualProvider.Underlying, UncheckedCloseable {
-    public static final FileHandle DIR = AuthServer.DIR.createSubDir("users");
-    public static final FileHandle SALTS = AuthServer.DIR.createSubDir("salts");
+    public static final FileHandle DIR = AuthServer.DATA.createSubDir("users");
+    public static final FileHandle SALTS = AuthServer.DATA.createSubDir("salts");
     private static final Logger logger = LogManager.getLogger();
     private static final Map<String, byte[]> salts = new ConcurrentHashMap<>();
 
