021965439088b5665111b9959ee6da531cc0c278
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-29 15:06:57 +0200

    move oauth module to guardian-webkit

--- auth/oauth/src/main/java/org/comroid/oauth/model/UserInfoProvider.java
+++
@@ -1,7 +0,0 @@
-package org.comroid.oauth.model;
-
-import org.comroid.uniform.node.UniNode;
-
-public interface UserInfoProvider {
-    UniNode getUserInfo();
-}
--- auth/oauth/src/main/java/org/comroid/oauth/resource/Resource.java
+++
@@ -1,9 +0,0 @@
-package org.comroid.oauth.resource;
-
-import org.comroid.api.Named;
-import org.comroid.api.UUIDContainer;
-import org.comroid.oauth.user.OAuthAuthorization;
-
-public interface Resource extends UUIDContainer, Named {
-    String generateAccessToken(OAuthAuthorization authorization);
-}
