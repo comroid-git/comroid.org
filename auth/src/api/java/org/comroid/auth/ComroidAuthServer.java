feeaaf033494330f2605d30c0521d3aec65dff70
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-05-06 00:06:37 +0200

    add initializer check

--- auth/src/api/java/org/comroid/auth/ComroidAuthServer.java
+++ auth/src/api/java/org/comroid/auth/ComroidAuthServer.java
@@ -1,6 +1,14 @@
 package org.comroid.auth;
 
+import org.apache.logging.log4j.LogManager;
+import org.comroid.api.ContextualProvider;
+
 public final class ComroidAuthServer {
+    static {
+        if (ContextualProvider.Base.ROOT.streamContextMembers(false).count() == 0)
+            LogManager.getLogger().error("Warning: Root Context has not been properly initialized. Expect Errors");
+    }
+
     private ComroidAuthServer() {
         throw new UnsupportedOperationException();
     }
