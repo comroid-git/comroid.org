c66946e64595742c1b6aff14d469c32582f12f5d
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-05-05 23:32:28 +0200

    create module and abstract user class

---
+++ auth/src/api/java/org/comroid/auth/AuthorizationUser.java
@@ -0,0 +1,202 @@
+package org.comroid.auth;
+
+import org.comroid.api.ContextualProvider;
+import org.comroid.api.EMailAddress;
+import org.comroid.common.info.MessageSupplier;
+import org.comroid.mutatio.model.Ref;
+import org.comroid.mutatio.ref.ReferenceList;
+import org.comroid.restless.CommonHeaderNames;
+import org.comroid.restless.HTTPStatusCodes;
+import org.comroid.restless.REST;
+import org.comroid.restless.body.BodyBuilderType;
+import org.comroid.uniform.model.Serializable;
+import org.comroid.uniform.node.UniNode;
+import org.comroid.uniform.node.UniObjectNode;
+import org.comroid.util.StandardValueType;
+import org.comroid.varbind.annotation.RootBind;
+import org.comroid.varbind.bind.GroupBind;
+import org.comroid.varbind.bind.VarBind;
+import org.comroid.varbind.container.DataContainerBase;
+import org.comroid.webkit.model.CookieProvider;
+import org.comroid.webkit.oauth.model.ValidityStage;
+import org.comroid.webkit.oauth.rest.OAuthEndpoint;
+import org.comroid.webkit.oauth.rest.request.TokenRevocationRequest;
+import org.comroid.webkit.oauth.user.OAuthAuthorization;
+
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public abstract class AuthorizationUser extends DataContainerBase<AuthorizationUser> implements ValidityStage, CookieProvider {
+    @RootBind
+    public static final GroupBind<AuthorizationUser> Type = new GroupBind<>("authorization-user");
+    public static final VarBind<AuthorizationUser, String, String, String> AUTHORIZATION_CODE
+            = Type.createBind("authorization-code")
+            .extractAs(StandardValueType.STRING)
+            .asIdentities()
+            .onceEach()
+            .setRequired()
+            .build();
+    public static final VarBind<AuthorizationUser, UniObjectNode, OAuthAuthorization.AccessToken, OAuthAuthorization.AccessToken> CURRENT_ACCESS_TOKEN
+            = Type.createBind("current-access-token")
+            .extractAsObject()
+            .andResolve(OAuthAuthorization.AccessToken::new)
+            .build();
+    public static final VarBind<AuthorizationUser, String, String, String> COOKIE
+            = Type.createBind("cookie")
+            .extractAs(StandardValueType.STRING)
+            .build();
+    public static final VarBind<AuthorizationUser, Long, Instant, Instant> EXPIRY
+            = Type.createBind("expiry")
+            .extractAs(StandardValueType.LONG)
+            .andRemap(Instant::ofEpochMilli)
+            .build();
+    public static final VarBind<AuthorizationUser, String, UUID, UUID> ID
+            = Type.createBind("uuid")
+            .extractAs(StandardValueType.STRING)
+            .andRemap(UUID::fromString)
+            .build();
+    public static final VarBind<AuthorizationUser, String, String, String> USERNAME
+            = Type.createBind("username")
+            .extractAs(StandardValueType.STRING)
+            .build();
+    public static final VarBind<AuthorizationUser, String, EMailAddress, EMailAddress> EMAIL
+            = Type.createBind("email")
+            .extractAs(StandardValueType.STRING)
+            .andRemap(EMailAddress::parse)
+            .build();
+    public final Ref<String> cookie = getComputedReference(COOKIE);
+    public final Ref<Instant> expiry = getComputedReference(EXPIRY);
+    public final Ref<UUID> uuid = getComputedReference(ID);
+    public final Ref<String> username = getComputedReference(USERNAME);
+    public final Ref<EMailAddress> email = getComputedReference(EMAIL);
+    public final Ref<String> authCode = getComputedReference(AUTHORIZATION_CODE);
+    public final Ref<OAuthAuthorization.AccessToken> accessToken = getComputedReference(CURRENT_ACCESS_TOKEN);
+    protected final CompletableFuture<Void> invalidation = new CompletableFuture<>();
+
+    public final String getAuthCode() {
+        return authCode.assertion("auth code");
+    }
+
+    public final String getToken() {
+        return accessToken.map(OAuthAuthorization.AccessToken::getToken).into(str -> "Bearer " + str);
+    }
+
+    public final boolean isValid() {
+        return !invalidation.isDone() && getToken() != null && expiry.test(it -> it.isAfter(Instant.now()));
+    }
+
+    public final UUID getUUID() {
+        return uuid.requireNonNull(unavailableMessage("UUID"));
+    }
+
+    public final String getUsername() {
+        return username.requireNonNull(unavailableMessage("Username"));
+    }
+
+    public final EMailAddress getEMailAddress() {
+        return email.requireNonNull(unavailableMessage("EMail"));
+    }
+
+    @Override
+    public final String getName() {
+        return username.isNull() ? "Unknown Username" : getUsername();
+    }
+
+    @Override
+    public final String getAlternateName() {
+        return getUUID().toString();
+    }
+
+    public AuthorizationUser(ContextualProvider context, Consumer<UniObjectNode> initialDataBuilder) {
+        super(context, initialDataBuilder);
+    }
+
+    private MessageSupplier unavailableMessage(String fieldName) {
+        return MessageSupplier.format("Field '%s' unavailable; UserData was never validated", fieldName);
+    }
+
+    @Override
+    public final boolean invalidate() {
+        return invalidateAuthorizationCode().join() == this;
+    }
+
+    public final CompletableFuture<? extends AuthorizationUser> invalidateAuthorizationCode() {
+        if (!isValid())
+            return CompletableFuture.completedFuture(this);
+        return getFromContext(REST.class)
+                .orElseGet(() -> new REST(this))
+                .request()
+                .method(REST.Method.POST)
+                .endpoint(OAuthEndpoint.TOKEN_REVOKE)
+                .buildBody(BodyBuilderType.OBJECT, obj -> {
+                    obj.put(TokenRevocationRequest.TOKEN, getAuthCode());
+                    obj.put(TokenRevocationRequest.TOKEN_HINT, "authorization_code");
+                })
+                .execute()
+                .thenApply(response -> {
+                    if (response.getStatusCode() != 200)
+                        throw response.toException();
+                    invalidation.complete(null);
+                    return this;
+                });
+    }
+
+    public final CompletableFuture<? extends AuthorizationUser> invalidateToken() {
+        if (!isValid())
+            return CompletableFuture.completedFuture(this);
+        return getFromContext(REST.class)
+                .orElseGet(() -> new REST(this))
+                .request()
+                .method(REST.Method.POST)
+                .endpoint(OAuthEndpoint.TOKEN_REVOKE)
+                .buildBody(BodyBuilderType.OBJECT, obj -> {
+                    obj.put(TokenRevocationRequest.TOKEN, getToken());
+                    obj.put(TokenRevocationRequest.TOKEN_HINT, "access_token");
+                })
+                .execute()
+                .thenApply(response -> {
+                    if (response.getStatusCode() != 200)
+                        throw response.toException();
+                    return this;
+                });
+    }
+
+    public final CompletableFuture<? extends AuthorizationUser> refreshAccessToken() {
+        return invalidateToken().thenCompose(it -> getFromContext(REST.class)
+                .orElseGet(() -> new REST(this))
+                .request((ctx, dat) -> new OAuthAuthorization.AccessToken(ctx, dat.asObjectNode()))
+                .method(REST.Method.POST)
+                .endpoint(OAuthEndpoint.TOKEN)
+                .buildBody(BodyBuilderType.OBJECT, obj -> obj.put("code", getAuthCode()))
+                .execute$deserializeSingle()
+                .thenApply(accessToken -> {
+                    getExtractionReference(CURRENT_ACCESS_TOKEN).compute(refs -> {
+                        if (refs == null)
+                            return ReferenceList.of(accessToken.toUniNode());
+                        refs.clear();
+                        refs.add(accessToken.toUniNode());
+                        return refs;
+                    });
+                    return this;
+                }));
+    }
+
+    public final CompletableFuture<? extends AuthorizationUser> validateUserInfo() {
+        return getFromContext(REST.class)
+                .orElseGet(() -> new REST(this))
+                .request()
+                .method(REST.Method.GET)
+                .endpoint(OAuthEndpoint.USER_INFO)
+                .addHeader(CommonHeaderNames.AUTHORIZATION, getToken())
+                .execute()
+                .thenApply(response -> {
+                    if (response.getStatusCode() != HTTPStatusCodes.OK)
+                        throw response.toException();
+                    UniNode data = response.getBody().into(Serializable::toUniNode);
+                    updateFrom(data.asObjectNode());
+                    return this;
+                });
+    }
+}
