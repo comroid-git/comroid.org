ac575cff94c9aeb62a1dbe3adf1ebbc95b05b27d
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-05-06 01:19:03 +0200

    add interface

--- auth/src/api/java/org/comroid/auth/user/User.java
+++ auth/src/api/java/org/comroid/auth/user/User.java
@@ -5,10 +5,11 @@ import org.comroid.auth.model.AuthEntity;
 import org.comroid.util.StandardValueType;
 import org.comroid.varbind.bind.GroupBind;
 import org.comroid.varbind.bind.VarBind;
+import org.comroid.webkit.oauth.client.Client;
 
 import java.util.UUID;
 
-public interface User extends AuthEntity {
+public interface User extends Client, AuthEntity {
     GroupBind<User> Type = AuthEntity.Type.subGroup("org.comroid.auth-user");
     VarBind<User, String, UUID, UUID> ID
             = Type.createBind("uuid")
