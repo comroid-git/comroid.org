977f50eadfd3d6a8c2a2a9d38ef4fb10f74bdf9e
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-05-06 01:44:17 +0200

    remove extends

--- auth/src/api/java/org/comroid/auth/user/User.java
+++ auth/src/api/java/org/comroid/auth/user/User.java
@@ -5,11 +5,10 @@ import org.comroid.auth.model.AuthEntity;
 import org.comroid.util.StandardValueType;
 import org.comroid.varbind.bind.GroupBind;
 import org.comroid.varbind.bind.VarBind;
-import org.comroid.webkit.oauth.client.Client;
 
 import java.util.UUID;
 
-public interface User extends Client, AuthEntity {
+public interface User extends AuthEntity {
     GroupBind<User> Type = AuthEntity.Type.subGroup("org.comroid.auth-user");
     VarBind<User, String, UUID, UUID> ID
             = Type.createBind("uuid")
