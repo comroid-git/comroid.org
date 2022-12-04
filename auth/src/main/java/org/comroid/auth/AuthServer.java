3a8f1765206c1b978ba57894716336bc57aa2687
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-04 17:44:07 +0100

    disable mail debugging

--- auth/src/main/java/org/comroid/auth/AuthServer.java
+++ auth/src/main/java/org/comroid/auth/AuthServer.java
@@ -67,7 +67,7 @@ public class AuthServer extends SpringBootServletInitializer implements WebMvcCo
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.debug", "true");
+        //props.put("mail.debug", "true");
 
         return sender;
     }
