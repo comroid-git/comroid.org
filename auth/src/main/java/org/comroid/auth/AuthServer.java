10517ff6edb3f89136bcfcdf4d9c3a3990321413
burdoto <kaleidox@comroid.org>
2023-03-19 18:15:04 +0100

    fix annotations

--- auth/src/main/java/org/comroid/auth/AuthServer.java
+++ auth/src/main/java/org/comroid/auth/AuthServer.java
@@ -1,7 +1,11 @@
 package org.comroid.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.comroid.api.io.FileHandle;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,12 +23,13 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -32,17 +37,18 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"org.comroid.auth.controller","org.comroid.auth.config"})
+@ComponentScan(basePackages = {"org.comroid.auth.controller", "org.comroid.auth.config"})
 @EntityScan(basePackages = "org.comroid.auth.entity")
 @ImportResource("classpath:beans.xml")
 @EnableJpaRepositories
 @ControllerAdvice
 @Configuration
-@EnableWebMvc
 public class AuthServer extends SpringBootServletInitializer implements WebMvcConfigurer {
     public static final FileHandle PATH_BASE = new FileHandle("/srv/auth/", true); // server path base
     public static final FileHandle DB_FILE = PATH_BASE.createSubFile("db.json");
     public static final FileHandle MAILER_FILE = PATH_BASE.createSubFile("mailer.json");
+    @Autowired
+    private InternalResourceViewResolver viewResolver;
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServer.class);
@@ -51,6 +57,30 @@ public class AuthServer extends SpringBootServletInitializer implements WebMvcCo
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(AuthServer.class);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor());
+    }
+
+    @Bean
+    public HandlerInterceptor interceptor() {
+        return new HandlerInterceptor() {
+            @Override
+            public void postHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+                if (modelAndView != null) {
+                    if (modelAndView.hasView() && modelAndView.getView() == null && modelAndView.getViewName() != null) {
+                        modelAndView.setView(viewResolver.resolveViewName(modelAndView.getViewName(), request.getLocale()));
+                        //request.setAttribute(WebUtils.INCLUDE_REQUEST_URI_ATTRIBUTE, true);
+                    }
+                }
+            }
+
+            @Override
+            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+            }
+        };
     }
 
     @Bean
