211e8cadbfb1b7812ff6df67dfab66787fc5a9fe
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-03-19 21:53:42 +0100

    run remote as debugging client

--- auth/remote.sh
+++ auth/remote.sh
@@ -1,3 +1,3 @@
 git pull
 gradle clean simplifyDist --refresh-dependencies
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar build/dist/auth.war
+java -agentlib:jdwp=transport=dt_socket,server=n,address=kaleidox.ddns.net:5005,suspend=y -jar build/dist/auth.war
